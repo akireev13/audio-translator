c7545e507932faddbd64192be1bd2686c1c3e2cd
akireev13 <akireev13@mail.ru>
2023-12-10 22:03:35 +0300

    added clients

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import time
 
 app = Flask(__name__)
 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './key.json'
+
+translate_client = translate_v2.Client()
+tts_client = texttospeech.TextToSpeechClient()
+
+SUPPORTED_GENDER_LANGUAGES = {
+    'pl': None,
+    'en': texttospeech.SsmlVoiceGender.NEUTRAL,  
+    'ru': texttospeech.SsmlVoiceGender.FEMALE, 
+    'es': None,  
+    'fr': None,  
+}
+
+
 @app.route('/')
 def default():
     return render_template('index.html', translated_text=None, audio_file=None)
